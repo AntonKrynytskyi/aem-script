b89bbfc8423ada1b0741b8d1f9c61885030e160c
Anton Krynytskyi <AntonKrynytskyi@gmail.com>
2019-08-04 02:17:42 +0300

    fn-004: add check if env has already  run, than don't run it again only show msg,that "env has already run"
    add check isRun already env or not
    add update.sh script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "[INFO] "
+sudo mkdir -p /usr/local/script/aem
+echo "[INFO] created /usr/local/script/aem folder"
+
+sudo cp aem /usr/local/script/aem/aem
+echo "[INFO] copy 'aem' script file to /usr/local/script/aem folder"
+
+sudo cp README.md /usr/local/script/aem/README.md
+echo "[INFO] move 'aem' script file to /usr/local/script/aem folder"
+
+sudo touch /usr/local/script/aem/cash.txt
+sudo chmod 777 /usr/local/script/aem/cash.txt
+sudo chmod 555 aem
+
+echo "[INFO] created cash.txt under /usr/local/script/aem folder"
+
+echo "[INFO] done"
+echo "[INFO] "
